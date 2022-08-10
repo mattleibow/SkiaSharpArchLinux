e1311f7a71898c11e24e768c17af6c02bd2f395b
Matthew Leibowitz <mattleibow@live.com>
2022-08-10 21:40:25 +0200

    More info

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using System.Runtime.InteropServices;
 using SkiaSharp;
 
 if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+{
+    Console.WriteLine(File.ReadAllText("/etc/os-release"));
     Process.Start("uname", "-r");
+}
 
 Console.WriteLine($"Hello, {SKImageInfo.PlatformColorType} World!");
